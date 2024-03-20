4ebff4bc4c2cd86e2d2561299ec0e74e0e32538d
devon45 <khanhduy.nguyen4520@gmail.com>
2024-03-20 10:48:03 +0700

    update docker build

--- Jenkins/vars/dockerBuild.groovy
+++ Jenkins/vars/dockerBuild.groovy
@@ -1,10 +1,9 @@
 def call(Map params){
-    echo "$params.path, $params.tag, $BUILD_NUMBER"
     sh """
         version=$(cat VERSION)
-        docker build -t $params.name $params.path
-        docker tag $params.name $params.name:$version
-        docker tag $params.name $params.name:$BUILD_NUMBER
+        docker build -t ${params.name} ${params.path}
+        docker tag ${params.name} ${params.name}:\$version
+        docker tag ${params.name} ${params.name}:${BUILD_NUMBER}
         docker images
     """
 }
